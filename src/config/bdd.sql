c3b0fd50d7ddcb6396857a5ee62757b08870dc67
Loïs Taulelle <lois.taulelle@ens-lyon.fr>
2021-02-11 10:49:54 +0100

    typos, +barrier

--- src/config/bdd.sql
+++ src/config/bdd.sql
@@ -1,45 +1,45 @@
 
-create table users(
+create table IF NOT EXISTS users (
     id_user bigserial PRIMARY KEY,
     login varchar(255) NOT NULL UNIQUE
     );
 
-create table hosts(
+create table IF NOT EXISTS hosts (
     id_host bigserial PRIMARY KEY,
     hostname varchar(255) NOT NULL UNIQUE
     );
 
-create table groupes(
+create table IF NOT EXISTS groupes (
     id_groupe bigserial PRIMARY KEY,
     group_name varchar(255) NOT NULL UNIQUE
     );
 
-create table metagroups(
+create table IF NOT EXISTS metagroups (
     id_metagroup bigserial PRIMARY KEY,
     metaname varchar(255) NOT NULL UNIQUE
     );
 
-create table clusters(
+create table IF NOT EXISTS clusters (
     id_cluster bigserial PRIMARY KEY,
     cluster_name varchar(255) NOT NULL UNIQUE
     );
 
-create table queues(
+create table IF NOT EXISTS queues (
     id_queue bigserial PRIMARY KEY,
     queue_name  varchar(255) NOT NULL UNIQUE
     );
 
 -- liaison users/groupes
-create table users_in_groupe(
+create table IF NOT EXISTS users_in_groupe (
     id_groupe bigint NOT NULL,
     id_user bigint NOT NULL,
     PRIMARY KEY (id_groupe, id_user),
-    FOREIGN KEY (id_groupe) REFERENCES groups (id_groupe),
+    FOREIGN KEY (id_groupe) REFERENCES groupes (id_groupe),
     FOREIGN KEY (id_user) REFERENCES users (id_user)
     );
 
 -- liaison hosts/queues
-create table hosts_in_queue(
+create table IF NOT EXISTS hosts_in_queue (
     id_queue bigint NOT NULL,
     id_host bigint NOT NULL,
     PRIMARY KEY (id_queue, id_host),
@@ -48,7 +48,7 @@ create table hosts_in_queue(
     );
 
 -- liaison hosts/clusters
-create table hosts_in_cluster(
+create table IF NOT EXISTS hosts_in_cluster (
     id_cluster bigint NOT NULL,
     id_host bigint NOT NULL,
     PRIMARY KEY (id_cluster, id_host),
@@ -57,7 +57,7 @@ create table hosts_in_cluster(
     );
 
 -- liaison group/metagroups
-create table groupe_in_meta(
+create table IF NOT EXISTS groupe_in_meta (
     id_metagroup bigint NOT NULL,
     id_groupe bigint NOT NULL,
     PRIMARY KEY (id_metagroup, id_groupe),
@@ -66,23 +66,23 @@ create table groupe_in_meta(
     );
 
 -- liaison user/metagroups
-create table user_in_meta(
+create table IF NOT EXISTS user_in_meta (
     id_metagroup bigint NOT NULL,
     id_user bigint NOT NULL,
-    PRIMARY KEY (id_metagroup, id_user)
+    PRIMARY KEY (id_metagroup, id_user),
     FOREIGN KEY (id_metagroup) REFERENCES metagroups (id_metagroup),
     FOREIGN KEY (id_user) REFERENCES users (id_user)
     );
 
 -- table centrale
-create table job_(
+create table IF NOT EXISTS job_ (
     id_queue bigint NOT NULL,
     id_host bigint NOT NULL,
     id_groupe bigint NOT NULL,
     id_user bigint NOT NULL,
     FOREIGN KEY (id_queue) REFERENCES queues (id_queue),
     FOREIGN KEY (id_host) REFERENCES hosts (id_host),
-    FOREIGN KEY (id_groupe) REFERENCES groups (id_groupe),
+    FOREIGN KEY (id_groupe) REFERENCES groupes (id_groupe),
     FOREIGN KEY (id_user) REFERENCES users (id_user),
     job_name varchar(128) NOT NULL,
     job_id integer NOT NULL,
@@ -100,5 +100,5 @@ create table job_(
     mem real NOT NULL,
     io real NOT NULL,
     maxvmem real NOT NULL,
-    PRIMARY KEY (id_queue, id_host, id_user, job_id, start_time, end_time),
+    PRIMARY KEY (id_queue, id_host, id_user, job_id, start_time, end_time)
     );
