8061318472e73451a7bb1fb72160e7d2db0b7b0f
Loïs Taulelle <lois.taulelle@ens-lyon.fr>
2021-02-11 16:16:23 +0100

    bien lire la doc :o)

--- src/sql/clusters_insert.sql
+++ src/sql/clusters_insert.sql
@@ -6,9 +6,9 @@
 --    cluster_name varchar(255) NOT NULL UNIQUE
 --    );
 
-INSERT INTO clusters(cluster_name) VALUES ('default') RETURNING id;
-INSERT INTO clusters(cluster_name) VALUES ('X5') RETURNING id;
-INSERT INTO clusters(cluster_name) VALUES ('E5') RETURNING id;
-INSERT INTO clusters(cluster_name) VALUES ('Lake') RETURNING id;
-INSERT INTO clusters(cluster_name) VALUES ('Epyc') RETURNING id;
--- INSERT INTO clusters(cluster_name) VALUES ('') RETURNING id;
+INSERT INTO clusters(cluster_name) VALUES ('default') RETURNING id_cluster;
+INSERT INTO clusters(cluster_name) VALUES ('X5') RETURNING id_cluster;
+INSERT INTO clusters(cluster_name) VALUES ('E5') RETURNING id_cluster;
+INSERT INTO clusters(cluster_name) VALUES ('Lake') RETURNING id_cluster;
+INSERT INTO clusters(cluster_name) VALUES ('Epyc') RETURNING id_cluster;
+-- INSERT INTO clusters(cluster_name) VALUES ('') RETURNING id_cluster;
--- src/sql/metagroupes_insert.sql
+++ src/sql/metagroupes_insert.sql
@@ -6,14 +6,14 @@
 --    meta_name varchar(255) NOT NULL UNIQUE
 --    );
 
-INSERT INTO metagroupes(meta_name) VALUES ('autres_ENS') RETURNING id;
-INSERT INTO metagroupes(meta_name) VALUES ('chimie_ENS') RETURNING id;
-INSERT INTO metagroupes(meta_name) VALUES ('chimie_UdL') RETURNING id;
-INSERT INTO metagroupes(meta_name) VALUES ('bio_ENS') RETURNING id;
-INSERT INTO metagroupes(meta_name) VALUES ('physique_ENS') RETURNING id;
-INSERT INTO metagroupes(meta_name) VALUES ('cral_ENS') RETURNING id;
-INSERT INTO metagroupes(meta_name) VALUES ('partenaires_ENS') RETURNING id;
-INSERT INTO metagroupes(meta_name) VALUES ('geol_ENS') RETURNING id;
-INSERT INTO metagroupes(meta_name) VALUES ('lmfa_ECL') RETURNING id;
-INSERT INTO metagroupes(meta_name) VALUES ('lip_ENS') RETURNING id;
--- INSERT INTO metagroupes(meta_name) VALUES ('') RETURNING id;
+INSERT INTO metagroupes(meta_name) VALUES ('autres_ENS') RETURNING id_metagroupe;
+INSERT INTO metagroupes(meta_name) VALUES ('chimie_ENS') RETURNING id_metagroupe;
+INSERT INTO metagroupes(meta_name) VALUES ('chimie_UdL') RETURNING id_metagroupe;
+INSERT INTO metagroupes(meta_name) VALUES ('bio_ENS') RETURNING id_metagroupe;
+INSERT INTO metagroupes(meta_name) VALUES ('physique_ENS') RETURNING id_metagroupe;
+INSERT INTO metagroupes(meta_name) VALUES ('cral_ENS') RETURNING id_metagroupe;
+INSERT INTO metagroupes(meta_name) VALUES ('partenaires_ENS') RETURNING id_metagroupe;
+INSERT INTO metagroupes(meta_name) VALUES ('geol_ENS') RETURNING id_metagroupe;
+INSERT INTO metagroupes(meta_name) VALUES ('lmfa_ECL') RETURNING id_metagroupe;
+INSERT INTO metagroupes(meta_name) VALUES ('lip_ENS') RETURNING id_metagroupe;
+-- INSERT INTO metagroupes(meta_name) VALUES ('') RETURNING id_metagroupe;
