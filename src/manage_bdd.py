a8c97c20ce2e7d3f067b3bbde3270cd26bd8fc52
Loïs Taulelle <lois.taulelle@ens-lyon.fr>
2021-02-12 16:18:19 +0100

    manage_bdd, indépendant de bdd_interact.py, avoid bdd.xml

--- src/manage_bdd.py
+++ src/manage_bdd.py
@@ -8,7 +8,7 @@
 # Copyright 2021 Loïs Taulelle
 
 """
-Test du module bdd_interact
+WITHOUT module bdd_interact
 
 create, or purge, database.
 
@@ -17,27 +17,23 @@ create, or purge, database.
 import argparse
 import logging
 import distutils.util
+import psycopg2
 
 import config
-from bdd_interact import BddTransaction
 
 log = logging.getLogger()
 stream_handler = logging.StreamHandler()
 log.addHandler(stream_handler)
-
-# bddXml = config.readBddXml()
-# bddXml = config.readBddXml(insert=False)  # pour la creation de la bdd
-# print(bddXml)
 
 
 def get_args(helper=False):
     """ read parser and return args (as args namespace),
         if helper=True, show usage() or help()
     """
-    parser = argparse.ArgumentParser(description='upload accounting file into database')
+    parser = argparse.ArgumentParser(description='(re)create/cleanup database (tests era)')
     parser.add_argument('-d', '--debug', action='store_true', help='toggle debug ON')
-    # parser.add_argument('-n', '--dryrun', action='store_true', help='dry run')
     parser.add_argument('-c', '--create', action='store_true', help='create tables')
+    parser.add_argument('-i', '--insert', action='store_true', help='insert preliminary tables')
     parser.add_argument('-p', '--purge', action='store_true', help='purge tables (drop)')
 
     if helper:
@@ -50,10 +46,7 @@ def get_args(helper=False):
 def query_yesno(question):
     """ Ask a yes/no question via input() and return 1 if yes, 0 elsewhere.
 
-    No default. See query_default_yes_no().
-
     import distutils(.util)
-
     """
     print("\n" + question + " [y/n]?")
     while True:
@@ -63,36 +56,44 @@ def query_yesno(question):
             print('Please reply "y" or "n".')
 
 
+def db_transaction(params, fichier, action=None):
+    """ using param, connect to db, execute SQL fichier """
+
+    gonogo = query_yesno(' '.join((action, fichier)))
+
+    try:
+        with psycopg2.connect(**params) as conn:
+            with conn.cursor() as curs:
+                if gonogo == 1:
+                    curs.execute(open(fichier, "r").read())
+
+    except (Exception, psycopg2.errors.SyntaxError) as Err:
+        print(Err)
+        logging.warning(Err)
+
+
 if __name__ == '__main__':
     """ """
 
     args = get_args()
-    log.debug(get_args(helper=True))
+
+    if args.debug:
+        log.setLevel('DEBUG')
+        log.debug(get_args(helper=True))
+
+    paramConDb = config.parserIni(filename='infodb.ini', section='postgresql')
+    log.debug(paramConDb)
 
     if args.create:
-        bddXml = config.readBddXml(insert=False)
+        db_transaction(paramConDb, 'sql/bdd.sql', action='create')
+
     elif args.purge:
-        bddXml = config.readBddXml()
+        db_transaction(paramConDb, 'sql/cleanup.sql', action='drop')
+
+    elif args.insert:
+        db_transaction(paramConDb, 'sql/clusters_insert.sql', action='insert')
+        db_transaction(paramConDb, 'sql/metagroupes_insert.sql', action='insert')
+
     else:
         get_args(helper=True)
         exit(1)
-
-    log.debug(bddXml)
-
-    e = BddTransaction()
-    e.configCon()  # configCon(filename, section)
-    e.establishCon()
-    # if(e.conn != None and bddXml != None):
-    if(e.conn is not None and bddXml is not None):
-        if args.create:
-            e.createTable(bddXml)
-            gonogo = query_yesno('create tables')
-
-        elif args.purge:
-            e.dropBdd(['job_', 'queue', 'clusters', 'metagroups', 'groupes', 'hosts', 'users'])
-            gonogo = query_yesno('drop tables')
-
-        if gonogo == 1:
-            e.executeCommand()
-
-        e.disconnectCon()
