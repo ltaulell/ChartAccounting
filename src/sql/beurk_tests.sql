78e38a5a6324c86de32035cdb2d21da36b5b625c
Loïs Taulelle <lois.taulelle@ens-lyon.fr>
2021-02-25 17:23:50 +0100

    PLEEEIIINNN de SQL !

--- src/sql/beurk_tests.sql
+++ src/sql/beurk_tests.sql
@@ -37,6 +37,19 @@ select * from job_ where start_time = (select max(start_time) from job_);
 select * from job_ where submit_time = (select max(submit_time) from job_);
 select * from job_ where end_time = (select max(end_time) from job_);
 
+-- dernier job inséré
+select max(id_job_) from job_;
+
+-- info sur le dernier job inséré
+SELECT q.queue_name, h.hostname, g.group_name, u.login, j.job_id, j.submit_time
+FROM job_ j
+INNER JOIN queues q ON j.id_queue = q.id_queue
+INNER JOIN hosts h ON j.id_host = h.id_host
+INNER JOIN groupes g ON j.id_groupe = g.id_groupe
+INNER JOIN users u ON j.id_user = u.id_user
+WHERE j.id_job_ = 
+    (SELECT max(id_job_) FROM job_)
+    ;
 
 -- # https://www.epochconverter.com/
 -- epoch (2012-01-01) 1325376000
@@ -48,6 +61,14 @@ select * from job_ where end_time = (select max(end_time) from job_);
 select sum(cpu) from job_ where id_groupe = 
     (select id_groupe from groupes where group_name = 'chimie') 
 AND start_time >= 1325376000 AND start_time <= 1356912000;
+-- total cpu d'un group (chimie) entre 01-01-2012 et 31-12-2012 (start_time)
+SELECT sum(job_.cpu)
+FROM job_, groupes
+WHERE job_.id_groupe = groupes.id_groupe
+  AND groupes.group_name = 'chimie'
+  AND job_.start_time >= 1325376000
+  AND job_.start_time <= 1356912000
+  ;
 
 -- total cpu, par groupe, entre 01-01-2012 et 31-12-2012 (start_time)
 SELECT g.group_name, sum(j.cpu) AS sum_value
@@ -59,6 +80,14 @@ WHERE j.id_groupe = ANY
 AND start_time >= 1325376000 AND start_time <= 1356912000 
 GROUP BY g.group_name, j.id_groupe
 ORDER BY sum_value DESC ;
+-- total cpu, par groupe, entre 01-01-2012 et 31-12-2012 (start_time), plus rapide
+SELECT groupes.group_name, sum(job_.cpu) AS sum_cpu
+FROM job_, groupes
+WHERE job_.id_groupe = groupes.id_groupe
+  AND start_time >= 1325376000 
+  AND start_time <= 1356912000
+GROUP BY groupes.group_name
+ORDER BY sum_cpu DESC;
 
 -- top ten, id_host, par cluster
 SELECT j.id_host, sum(ru_utime) AS sum_value
@@ -74,6 +103,22 @@ WHERE id_host = ANY
     )
 GROUP BY j.id_host
 ORDER BY sum_value DESC
+LIMIT 10 ;
+
+-- pareil, plus rapide
+SELECT hosts.hostname, 
+    sum(job_.cpu) AS sum_cpu, 
+    sum(job_.ru_utime) AS sum_utime
+FROM job_,
+    hosts,
+    hosts_in_clusters,
+    clusters
+WHERE job_.id_host = hosts.id_host AND 
+    hosts.id_host = hosts_in_clusters.id_host AND
+    hosts_in_clusters.id_cluster = clusters.id_cluster AND
+    clusters.cluster_name = 'E5'
+GROUP BY hosts.hostname, job_.id_host
+ORDER BY sum_cpu DESC
 LIMIT 10 ;
 
 -- top ten, with hostname, par cluster
@@ -92,3 +137,25 @@ WHERE j.id_host = ANY
 GROUP BY h.hostname, j.id_host
 ORDER BY sum_value DESC
 LIMIT 10 ;
+
+-- top ten, with cluster_name, hostname, tout clusters confondus
+SELECT 
+  clusters.cluster_name, 
+  hosts.hostname, 
+  sum(job_.cpu) AS sum_cpu_value,
+  sum(job_.ru_utime) AS sum_cpu_utime
+FROM 
+  job_, 
+  clusters, 
+  hosts_in_clusters, 
+  hosts
+WHERE 
+  hosts_in_clusters.id_cluster = clusters.id_cluster AND
+  hosts.id_host = job_.id_host AND
+  hosts.id_host = hosts_in_clusters.id_host
+GROUP BY
+  clusters.cluster_name, hosts.hostname
+ORDER BY
+  sum_cpu_value DESC
+LIMIT 10;
+
