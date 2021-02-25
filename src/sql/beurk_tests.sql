b3eca836ee6e897c2ea1b0e07f17bd54ac31c956
Loïs Taulelle <lois.taulelle@ens-lyon.fr>
2021-02-25 10:20:31 +0100

    add moar SQL tests

--- src/sql/beurk_tests.sql
+++ src/sql/beurk_tests.sql
@@ -20,25 +20,47 @@ ALTER TABLE job_ ALTER COLUMN mem DROP NOT NULL;
 ALTER TABLE job_ ALTER COLUMN io DROP NOT NULL;
 ALTER TABLE job_ ALTER COLUMN maxvmem DROP NOT NULL;
 
-
--- explain
+-- explain, pour les query plan
+-- voir où ça bouffe du temps
 
 select sum(ru_utime) from job_ where id_user = (select id_user from users where login = 'cmichel');
+
 select sum(ru_utime) from job_ where id_groupe = (select id_groupe from groupes where group_name = 'icbms')
 
 select max(ru_wallclock) from job_ where id_groupe = (select id_groupe from groupes where group_name = 'icbms');
+
 select max(ru_wallclock) from job_ where id_groupe = (select id_groupe from groupes where group_name = 'chimie');
+
 select max(slots) from job_ where id_groupe = (select id_groupe from groupes where group_name = 'chimie');
+
+select * from job_ where start_time = (select max(start_time) from job_);
+select * from job_ where submit_time = (select max(submit_time) from job_);
+select * from job_ where end_time = (select max(end_time) from job_);
+
 
 -- # https://www.epochconverter.com/
 -- epoch (2012-01-01) 1325376000
 -- epoch (2012-12-31) 1356912000
 
-select sum(cpu) from job_ where id_groupe = (select id_groupe from groupes where group_name = 'chimie') AND start_time >= 1325376000 AND start_time <= 1356912000;
+-- https://sql.sh/cours/jointures
 
--- essais
+-- total cpu d'un group (chimie) entre 01-01-2012 et 31-12-2012 (start_time)
+select sum(cpu) from job_ where id_groupe = 
+    (select id_groupe from groupes where group_name = 'chimie') 
+AND start_time >= 1325376000 AND start_time <= 1356912000;
 
--- top ten, id_host
+-- total cpu, par groupe, entre 01-01-2012 et 31-12-2012 (start_time)
+SELECT g.group_name, sum(j.cpu) AS sum_value
+FROM job_ j
+INNER JOIN groupes g ON j.id_groupe = g.id_groupe
+WHERE j.id_groupe = ANY 
+    (SELECT id_groupe
+    FROM groupes)
+AND start_time >= 1325376000 AND start_time <= 1356912000 
+GROUP BY g.group_name, j.id_groupe
+ORDER BY sum_value DESC ;
+
+-- top ten, id_host, par cluster
 SELECT j.id_host, sum(ru_utime) AS sum_value
 FROM job_ j
 WHERE id_host = ANY 
@@ -54,9 +76,8 @@ GROUP BY j.id_host
 ORDER BY sum_value DESC
 LIMIT 10 ;
 
--- https://sql.sh/cours/jointures
--- top ten, with hostname
-SELECT h.hostname, sum(j.ru_utime) AS sum_value
+-- top ten, with hostname, par cluster
+SELECT h.hostname, sum(j.cpu), sum(j.ru_utime) AS sum_value
 FROM job_ j
 INNER JOIN hosts h ON j.id_host = h.id_host
 WHERE j.id_host = ANY 
