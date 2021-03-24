04abafe444d2c9325037feb866f9b27301954cd9
Loïs Taulelle <lois.taulelle@ens-lyon.fr>
2021-03-24 16:50:03 +0100

    add more sql (user part)
    add queries for memory usage, refilter this file, same order as the charts list

--- src/sql/proper_queries_user.sql
+++ src/sql/proper_queries_user.sql
@@ -7,6 +7,29 @@
 -- year = 2012 (in epoch, 01-01-2012, 01-01-2013)
 --
 
+-- composite cmichel vs chimie
+-- nb de jobs réussis, nb d'heures consommées d'un user (cmichel, 2012)
+SELECT users.login, COUNT(job_.id_job_) AS nb_job, SUM(job_.cpu) AS sum_cpu
+FROM job_, users
+WHERE job_.id_user = users.id_user
+    AND users.login = 'cmichel'
+    AND (job_.failed = 0 OR job_.exit_status = 0)
+    AND job_.start_time >= 1325376000
+    AND job_.start_time <= 1356998400
+GROUP BY users.login ;
+-- nb de jobs réussis, nb d'heures d'un groupe (chimie, 2012) moins un user particulier
+SELECT groupes.group_name, COUNT(job_.id_job_) AS nb_job, SUM(job_.cpu) AS sum_cpu
+FROM job_, groupes, users
+WHERE job_.id_groupe = groupes.id_groupe
+    AND groupes.group_name = 'chimie'
+    AND (job_.failed = 0 OR job_.exit_status = 0)
+    AND job_.start_time >= 1325376000
+    AND job_.start_time <= 1356998400
+    AND job_.id_user = users.id_user
+    AND users.login != 'cmichel'
+GROUP BY groupes.id_groupe ;
+
+-- taux de réussite
 -- nb de jobs réussis d'un user (cmichel, 2012)
 SELECT users.login, COUNT(job_.id_job_)
 FROM job_, users
@@ -27,6 +50,7 @@ WHERE job_.id_user = users.id_user
     AND job_.start_time <= 1356998400
 GROUP BY users.login ;
 
+-- execution time
 -- max, avg, min for ru_wallclock, user cmichel, 2012
 SELECT
     users.login,
@@ -42,7 +66,7 @@ WHERE
     AND job_.start_time >= 1325376000
     AND job_.start_time <= 1356998400
 GROUP BY users.login ;
--- jobs au dessus de avg, user cmichel, 2012
+-- jobs au dessus de avg(ru_wallclock), user cmichel, 2012
 SELECT 
     users.login, COUNT(job_.id_job_)
 FROM 
@@ -68,7 +92,7 @@ WHERE
     GROUP BY users.login
     )
 GROUP BY users.login ;
--- jobs en dessous de avg, user cmichel, 2012
+-- jobs en dessous de avg(ru_wallclock), user cmichel, 2012
 SELECT 
     users.login, COUNT(job_.id_job_)
 FROM 
@@ -94,29 +118,6 @@ WHERE
     GROUP BY users.login
     )
 GROUP BY users.login ;
-
--- composite cmichel vs chimie
--- nb de jobs réussis, nb d'heures d'un user (cmichel, 2012)
-SELECT users.login, COUNT(job_.id_job_) AS nb_job, SUM(job_.cpu) AS sum_cpu
-FROM job_, users
-WHERE job_.id_user = users.id_user
-    AND users.login = 'cmichel'
-    AND (job_.failed = 0 OR job_.exit_status = 0)
-    AND job_.start_time >= 1325376000
-    AND job_.start_time <= 1356998400
-GROUP BY users.login ;
--- nb de jobs réussis, nb d'heures d'un groupe (chimie, 2012) moins un user particulier
-SELECT groupes.group_name, COUNT(job_.id_job_) AS nb_job, SUM(job_.cpu) AS sum_cpu
-FROM job_, groupes, users
-WHERE job_.id_groupe = groupes.id_groupe
-    AND groupes.group_name = 'chimie'
-    AND (job_.failed = 0 OR job_.exit_status = 0)
-    AND job_.start_time >= 1325376000
-    AND job_.start_time <= 1356998400
-    AND job_.id_user = users.id_user
-    AND users.login != 'cmichel'
-GROUP BY groupes.id_groupe ;
-
 -- nb jobs réussis, user cmichel, 2012, durée inférieure à 1 jour (86400)
 SELECT users.login, COUNT(job_.id_job_)
 FROM job_, users
@@ -158,5 +159,160 @@ WHERE job_.id_user = users.id_user
     AND job_.start_time >= 1325376000
     AND job_.start_time <= 1356998400
     AND job_.ru_wallclock > 18144000
+GROUP BY users.login ;
+
+-- memory usage
+-- max, avg, min for maxvmem, user cmichel, 2012
+SELECT
+    users.login,
+    MAX(job_.maxvmem) AS max_mem,
+    AVG(job_.maxvmem) AS avg_mem,
+    MIN(job_.maxvmem) AS min_mem
+FROM 
+    job_, users
+WHERE
+    job_.id_user = users.id_user
+    AND users.login = 'cmichel'
+    AND (job_.failed = 0 OR job_.exit_status = 0)
+    AND job_.start_time >= 1325376000
+    AND job_.start_time <= 1356998400
+GROUP BY users.login ;
+-- jobs au dessus de avg(maxvmem), user cmichel, 2012
+SELECT 
+    users.login, COUNT(job_.id_job_)
+FROM 
+    job_, users
+WHERE 
+    job_.id_user = users.id_user
+    AND users.login = 'cmichel'
+    AND (job_.failed = 0 OR job_.exit_status = 0)
+    AND job_.start_time >= 1325376000
+    AND job_.start_time <= 1356998400
+    -- avg donné par requête imbriquée
+    AND job_.maxvmem > (
+    SELECT 
+        AVG(job_.maxvmem)
+    FROM 
+        job_, users
+    WHERE 
+        job_.id_user = users.id_user
+        AND users.login = 'cmichel'
+        AND (job_.failed = 0 OR job_.exit_status = 0)
+        AND job_.start_time >= 1325376000
+        AND job_.start_time <= 1356998400
+    GROUP BY users.login
+    )
+GROUP BY users.login ;
+-- jobs en dessous de avg(maxvmem), user cmichel, 2012
+SELECT 
+    users.login, COUNT(job_.id_job_)
+FROM 
+    job_, users
+WHERE 
+    job_.id_user = users.id_user
+    AND users.login = 'cmichel'
+    AND (job_.failed = 0 OR job_.exit_status = 0)
+    AND job_.start_time >= 1325376000
+    AND job_.start_time <= 1356998400
+    -- avg donné par requête imbriquée
+    AND job_.maxvmem < (
+    SELECT 
+        AVG(job_.maxvmem)
+    FROM 
+        job_, users
+    WHERE 
+        job_.id_user = users.id_user
+        AND users.login = 'cmichel'
+        AND (job_.failed = 0 OR job_.exit_status = 0)
+        AND job_.start_time >= 1325376000
+        AND job_.start_time <= 1356998400
+    GROUP BY users.login
+    )
+GROUP BY users.login ;
+-- nb jobs réussis, user cmichel, 2012, maxvmem inférieure à 1G (1073741824)
+SELECT users.login, COUNT(job_.id_job_)
+FROM job_, users
+WHERE job_.id_user = users.id_user
+    AND users.login = 'cmichel'
+    AND (job_.failed = 0 OR job_.exit_status = 0)
+    AND job_.start_time >= 1325376000
+    AND job_.start_time <= 1356998400
+    AND job_.maxvmem < 1073741824
+GROUP BY users.login ;
+-- nb jobs réussis, user cmichel, 2012, maxvmem entre 1G (1073741824) et 4G (4294967296)
+SELECT users.login, COUNT(job_.id_job_)
+FROM job_, users
+WHERE job_.id_user = users.id_user
+    AND users.login = 'cmichel'
+    AND (job_.failed = 0 OR job_.exit_status = 0)
+    AND job_.start_time >= 1325376000
+    AND job_.start_time <= 1356998400
+    AND job_.maxvmem > 1073741824
+    AND job_.maxvmem < 4294967296
+GROUP BY users.login ;
+-- nb jobs réussis, user cmichel, 2012, maxvmem entre 4G (4294967296) et 8G (8589934592)
+SELECT users.login, COUNT(job_.id_job_)
+FROM job_, users
+WHERE job_.id_user = users.id_user
+    AND users.login = 'cmichel'
+    AND (job_.failed = 0 OR job_.exit_status = 0)
+    AND job_.start_time >= 1325376000
+    AND job_.start_time <= 1356998400
+    AND job_.maxvmem > 4294967296
+    AND job_.maxvmem < 8589934592
+GROUP BY users.login ;
+-- nb jobs réussis, user cmichel, 2012, maxvmem entre 8G (8589934592) et 16G (17179869184)
+SELECT users.login, COUNT(job_.id_job_)
+FROM job_, users
+WHERE job_.id_user = users.id_user
+    AND users.login = 'cmichel'
+    AND (job_.failed = 0 OR job_.exit_status = 0)
+    AND job_.start_time >= 1325376000
+    AND job_.start_time <= 1356998400
+    AND job_.maxvmem > 8589934592
+    AND job_.maxvmem < 17179869184
+GROUP BY users.login ;
+-- nb jobs réussis, user cmichel, 2012, maxvmem entre 16G (17179869184) et 32G (34359738368)
+SELECT users.login, COUNT(job_.id_job_)
+FROM job_, users
+WHERE job_.id_user = users.id_user
+    AND users.login = 'cmichel'
+    AND (job_.failed = 0 OR job_.exit_status = 0)
+    AND job_.start_time >= 1325376000
+    AND job_.start_time <= 1356998400
+    AND job_.maxvmem > 17179869184
+    AND job_.maxvmem < 34359738368
+GROUP BY users.login ;
+-- nb jobs réussis, user cmichel, 2012, maxvmem entre 32G (34359738368) et 64G (68719476736)
+SELECT users.login, COUNT(job_.id_job_)
+FROM job_, users
+WHERE job_.id_user = users.id_user
+    AND users.login = 'cmichel'
+    AND (job_.failed = 0 OR job_.exit_status = 0)
+    AND job_.start_time >= 1325376000
+    AND job_.start_time <= 1356998400
+    AND job_.maxvmem > 34359738368
+    AND job_.maxvmem < 68719476736
+GROUP BY users.login ;
+-- nb jobs réussis, user cmichel, 2012, maxvmem entre 64G (68719476736) et 128G (137438953472)
+SELECT users.login, COUNT(job_.id_job_)
+FROM job_, users
+WHERE job_.id_user = users.id_user
+    AND users.login = 'cmichel'
+    AND (job_.failed = 0 OR job_.exit_status = 0)
+    AND job_.start_time >= 1325376000
+    AND job_.start_time <= 1356998400
+    AND job_.maxvmem > 68719476736
+    AND job_.maxvmem < 137438953472
+GROUP BY users.login ;
+-- nb jobs réussis, user cmichel, 2012, maxvmem supérieure à 128G (137438953472)
+SELECT users.login, COUNT(job_.id_job_)
+FROM job_, users
+WHERE job_.id_user = users.id_user
+    AND users.login = 'cmichel'
+    AND (job_.failed = 0 OR job_.exit_status = 0)
+    AND job_.start_time >= 1325376000
+    AND job_.start_time <= 1356998400
+    AND job_.maxvmem > 137438953472
 GROUP BY users.login ;
 
